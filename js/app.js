c5e0a0623129e4897a4389c01bdee69a95739d34
Gsunn <gsun975@gmail.com>
2022-01-16 11:19:00 +0100

    Eliminar cita

--- js/app.js
+++ js/app.js
@@ -20,6 +20,10 @@ class Citas{
     agregarCita(cita){
         this.citas =[...this.citas, cita]
     }
+
+    eliminarCita(id){
+        this.citas = this.citas.filter( cita => cita.id !== id)
+    }
 }
 
 class UI{
@@ -83,6 +87,13 @@ class UI{
             const sintomasParrafo = document.createElement('p')
             sintomasParrafo.innerHTML = `<span class="font-weight-bolder">Sintomas: </span> ${sintomas}`
 
+              // Agregar un botón de eliminar...
+              const btnEliminar = document.createElement('button');
+              btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
+              btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+              btnEliminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>'
+  
+
             
             // Agregar parrados
             divCita.appendChild(mascotaParrafo)
@@ -91,6 +102,7 @@ class UI{
             divCita.appendChild(fechaParrafo)
             divCita.appendChild(horaParrafo)
             divCita.appendChild(sintomasParrafo)
+            divCita.appendChild(btnEliminar)
 
 
             // Agregar al DOM
@@ -174,4 +186,13 @@ function reiniciarObjeto() {
     citaObj.fecha = ''
     citaObj.hora = ''
     citaObj.sintomas = ''
+}
+
+function eliminarCita(id){
+    // eliminar cita
+    administrarCitas.eliminarCita(id)
+    // mostrar cita
+    ui.imprimirAlerta('La cita se elimino correctamente.', 'success')
+    //mostrar citas
+    ui.imprimirCitas(administrarCitas)
 }
